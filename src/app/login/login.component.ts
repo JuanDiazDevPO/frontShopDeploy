d00e7117144d6a3c5fa1bdc94740d9ff93a7e41c
XxSantiagoxX15 <santiagomelo15@hotmail.com>
2024-11-07 14:36:10 -0500

    login conectado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,20 +3,51 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule,MatCardModule,MatButtonModule ],
+  imports: [MatInputModule, MatCardModule, MatButtonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private router:Router){}
-  navReg ():void {
+  email: string = '';
+  passwordValue: string = '';
+
+  constructor(private router: Router, private loginService: LoginService) {}
+
+  navReg(): void {
     this.router.navigate(['register']);
   }
-  navMenu ():void {
+
+  navMenu(): void {
     this.router.navigate(['menu']);
+  }
+
+  onLogin(): void {
+    const loginRequest = {
+      email: this.email,
+      contraseña: this.passwordValue,
+    };
+
+    // Ver lo que se va a enviar al backend
+    console.log('Enviando al backend:', loginRequest);
+
+    this.loginService.login(loginRequest).subscribe({
+      next: (response) => {
+        // Aquí la respuesta será un texto plano
+        alert(response);  // Mostrar el mensaje de texto recibido del backend
+        if (response === 'Inicio de sesión exitoso') {
+          this.navMenu();
+        }
+      },
+      error: (error) => {
+        // Manejar cualquier error en la solicitud
+        alert(error.error || 'Error al iniciar sesión');
+      },
+    });
   }
 }
